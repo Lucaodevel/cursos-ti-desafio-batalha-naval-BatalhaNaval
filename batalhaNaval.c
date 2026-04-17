bef76ccab1eb8724bd397e1cb8eb7f8fc89983c0
Lucaodevel <lucas.martins.a@outlook.com>
2026-04-17 20:50:33 +0000

    Finalizado Batalha Naval - Nivel Novato

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,14 +1,90 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+int main(){
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    // Declaração das Variaveis
+    int areaTab = 10;
+    int areaNavio = 3;
+
+    // Matriz do Tabuleiro iniciando em '0'
+    int tabuleiro[10][10];
+
+    for (int i = 0; i < areaTab; i++) {
+        for(int j = 0; j < areaTab; j++) {
+            tabuleiro[i][j] = 0;            
+        }
+    }
+
+    // Colocando navios no tabuleiro
+    int navioHorizontal[3] = {3, 3, 3};
+    int navioVertical[3] = {3, 3, 3};
+
+    //Coordenadas Iniciais
+    int linhaH = 2, colunaH = 4; // Horizontal
+    int linhaV = 5, colunaV = 1; // Vertical
+
+    // Navio Horizontal
+    if (colunaH + areaNavio > areaTab) {
+        printf("Erro: Navio horizontal fora do tabuleiro!\n");
+        return 1;
+    }
+
+    // Verifica sobreposição
+    for (int i = 0; i < areaNavio; i++) {
+        if (tabuleiro[linhaH][colunaH + i] != 0) {
+            printf("Erro: Sobreposição no navio horizontal!\n");
+            return 1;
+        }
+    }
+
+    // Posição navio horizontal
+
+    for (int i = 0; i < areaNavio; i++) {
+        tabuleiro[linhaH][colunaH + i] = navioHorizontal[i];
+    }
+
+    // Navio Vertical
+    if (linhaV + areaNavio > areaTab) {
+        printf("Erro: Navio vertical fora do tabuleiro!\n");
+        return 1;
+    }
+
+    // Verifica sobreposição
+    for (int i = 0; i < areaNavio; i++) {
+        if (tabuleiro[linhaV + i][colunaV] != 0) {
+            printf("Erro: Sobreposição no navio vertical!\n");
+            return 1;
+        }
+    }
+
+    // Posiciona navio vertical
+    for (int i = 0; i < areaNavio; i++) {
+        tabuleiro[linhaV + i][colunaV] = navioVertical[i];
+    }
+
+    // -------- EXIBIÇÃO --------
+    printf("\nTabuleiro Batalha Naval:\n");
+
+    printf("\n   ");
+
+    // Letras das colunas
+    for (int j = 0; j < areaTab; j++) {
+        printf("%c ", 'A' + j);
+    }
+    printf("\n");
+
+    // Linhas + conteúdo
+    for(int i = 0; i < areaTab; i++) {
+        printf("%2d ", i + 1); // linhas de 1 a 10
+
+        for(int j = 0; j < areaTab; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
@@ -35,6 +111,3 @@ int main() {
     // 0 0 1 0 0
     // 1 1 1 1 1
     // 0 0 1 0 0
-
-    return 0;
-}
